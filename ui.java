b1e99c65b07449db2230d68f182689bfe35f84dc
akshat-khare <akshat.khare08@gmail.com>
2018-01-15 21:10:27 +0530

    validity completing

--- ui.java
+++ ui.java
@@ -32,4 +32,176 @@ public class ui{
             }
         }
     }
+    public static boolean checkValidV2(String[][] obj, int x, int y, int player){
+        String sameone = player+"";
+        String otherone = (3- (player))+"";
+        boolean row = False;
+        boolean column =False;
+        boolean diagonalrightslash = False;
+        
+        if(obj[x][y]=="1"|obj[x][y]=="2"){
+            return False;
+        }
+        //check in the row
+        if(x==0){
+            //check right
+            if(obj[1][y]!=otherone){
+                row=False;
+            }else{
+                for(int i=2;i<8;i++){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                row=False;
+            }
+        }else if(x==7){
+            //check left
+            if(obj[6][y]!=otherone){
+                row=False;
+            }else{
+                for(int i=6;i>=0;i--){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                row=False;
+            }
+        }else{
+            //checkboth
+            if(obj[x+1][y]!=otherone  && obj[x-1][y]!=otherone){
+                row = False;
+            }else if(obj[x+1][y]!=otherone){
+                //checkonly left
+                for(int i=x-2;i>=0;i--){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                row= False;
+            }else if(obj[x-1][y]!=otherone){
+                //checkonly right
+                for(int i=x+2;i<8;i++){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                row=False;
+            }else{
+                //checkboth
+                for(int i=x-2;i>=0;i--){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                for(int i=x+2;i<8;i++){
+                    if(obj[i][y]==sameone){
+                        return True;
+                    }else if(obj[i][y]=="_"){
+                        row = False;
+                        break;
+                    }
+                }
+                row=False;
+            }
+        }
+        //checkcolumn
+        if(y==0){
+            //check down
+            if(obj[x][1]!=otherone){
+                column=False;
+            }else{
+                for(int i=2;i<8;i++){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                column=False;
+            }
+        }else if(x==7){
+            //check up
+            if(obj[x][6]!=otherone){
+                column=False;
+            }else{
+                for(int i=6;i>=0;i--){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                column=False;
+            }
+        }else{
+            //checkboth
+            if(obj[x][y+1]!=otherone  && obj[x][y-1]!=otherone){
+                column = False;
+            }else if(obj[x][y+1]!=otherone){
+                //checkonly up
+                for(int i=y-2;i>=0;i--){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                column= False;
+            }else if(obj[x][y-1]!=otherone){
+                //checkonly down
+                for(int i=y+2;i<8;i++){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                column=False;
+            }else{
+                //checkboth
+                for(int i=y-2;i>=0;i--){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                for(int i=y+2;i<8;i++){
+                    if(obj[x][i]==sameone){
+                        return True;
+                    }else if(obj[x][i]=="_"){
+                        column = False;
+                        break;
+                    }
+                }
+                column=False;
+            }
+        }
+        //checkdiagonal 
+
+
+
+        return False;
+    }
 }
