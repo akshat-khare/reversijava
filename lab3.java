679526f741d32ddbf71359f58aa8439ca3f93fe1
DivyanshuSaxena <dssaxena2011@gmail.com>
2018-01-15 14:53:02 +0530

    initial commit

---
+++ lab3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main
+{
+    static int input[][] = new int[4][4];
+    static int output[][] = new int[15][40];
+    static int move;
+    static int player1Led, player2Led;
+    static int player1btn, player2btn;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;i++){
+                if()
+            }
+        }
+    }   
+}
